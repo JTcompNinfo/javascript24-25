79da8ddd12aca6261a71e1fc66492037a609b735
JTcompNinfo <cte-joel.dinovo@sllboces.org>
2025-01-08 18:31:31 +0000

    google console

---
+++ exJSVariables.js
@@ -0,0 +1,11 @@
+//Introduction to JavaScript Variables
+
+// 1. Using var (older way of declaring variables, avoid in mordern code)
+var age = 51; // This is number variable, specifically  an Integer
+
+console.log("Using var: the age is " + age)
+
+
+
+// Change the value of a variable with let
+
